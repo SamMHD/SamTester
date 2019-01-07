196b14ebf44159ef5393ac81cf3a083b4d764a91
SamMHD <46444723+SamMHD@users.noreply.github.com>
2019-01-07 14:54:41 +0330

    Update SamTESTERv2.cpp

--- SamTESTERv2.cpp
+++ SamTESTERv2.cpp
@@ -15,28 +15,36 @@ int main() {
 	cout << "Please make Sure that tester and 3 files are in SAME directory\n";
 	cout << "\nHow many test case do you want to generate and test?\nn = " << flush;
 	cin >> n; cout << endl;
+	
 	system ("g++ checker.cpp -std=c++11 -O2 -o check.out");
 	system ("g++ cprog.cpp -std=c++11 -O2 -o result.out");
 	system ("g++ generator.cpp -std=c++11 -O2 -o gen.out");
 	system ("echo > check_ans.txt");
 	system ("echo > result_ans.txt");
 	system ("echo > test_case.txt");
+	
 	for (int i = 0; i < n; i++) {
 		cout << "TESTING test #" << i + 1 << ": " << flush;
 		system("rm check_ans.txt");
 		system("rm result_ans.txt");
 		system("rm test_case.txt");
 		cout << "                  \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b" << flush;
+		
 		system("./gen.out > test_case.txt"); cout << '~' << flush;
 		system("./check.out < test_case.txt > check_ans.txt"); cout << '~' << flush;
 		system("./result.out < test_case.txt > result_ans.txt"); cout << '~' << flush;
+		
 		if (!same()) {
 			cout << "Program TEST RESULT: WrongAnswer" << endl;
 			cout << "Verdict: test case found! (saved: test_case.txt)" << endl;
 			remove_profile();
 			return 0;
-		} else
-			cout << "Passed" << flush << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+		} else {
+			cout << "Passed" << flush; 
+			for (int i = 0; i < 30; i++)
+				cout << "\b";
+			cout << flush;
+		}
 	}
 	remove_profile();
 	system("rm test_case.txt");
